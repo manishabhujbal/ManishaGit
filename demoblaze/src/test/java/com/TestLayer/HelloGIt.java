93fbfbd8dfad7ff333cdedd6720e5465252e9412
manishabhujbal <118868386+manishabhujbal@users.noreply.github.com>
2022-11-23 14:58:32 +0530

    basic project structure
    Hi all,
    I have created basic project structure and start working on individual task.
    Regards,
    
    Co-Authored-By: manishabhujbal <118868386+ManishaBhujbal@users.noreply.github.com>

---
+++ demoblaze/src/test/java/com/TestLayer/HelloGIt.java
@@ -0,0 +1,12 @@
+package com.TestLayer;
+
+import org.testng.annotations.Test;
+
+public class HelloGIt {
+
+@Test	
+	public void demo()
+	{
+		System.out.println("********HelloGit************");
+	}
+}
